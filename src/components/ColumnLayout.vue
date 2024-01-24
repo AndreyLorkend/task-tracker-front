a4306245c6b5c8d70b6841047566b3f43db5fb49
ixmn <zametaeva.dasha@gmail.com>
2024-01-24 17:48:42 +0400

    feat/wip (components) добавить layouts

--- src/components/ColumnLayout.vue
+++ src/components/ColumnLayout.vue
@@ -1,3 +1,30 @@
 <template lang="pug">
 //- Отрисовка карточек
+.column-layout--container
+  div
+    app-title column name
+  div
+    app-button.column-layout--button Добавить новую задачу
 </template>
+
+<style scoped>
+.column-layout--container {
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 400px;
+  height: 750px;
+
+  border-radius: 30px;
+  background: #FFF;
+  padding: 30px;
+
+  justify-content: space-between;
+}
+
+.column-layout--button {
+  background-color: var(--app-color-background);
+}
+</style>
--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,3 +1,33 @@
+<script setup>
+import ColumnLayout from "./ColumnLayout.vue";
+</script>
+
 <template lang="pug">
-//- Отрисовка колонок
+.dashboard-layout-header 
+  .dashboard-layout-header--text タスクリスト
+  app-button Добавить новую колонку
+.dashboard-layout-column
+  column-layout
 </template>
+
+<style scoped>
+.dashboard-layout-header {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  padding: 25px;
+}
+.dashboard-layout-header--text {
+  color: var(--app-text-color);
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.dashboard-layout-column {
+  display: flex;
+  gap: 25px;
+  margin: 0 25px;
+}
+</style>
