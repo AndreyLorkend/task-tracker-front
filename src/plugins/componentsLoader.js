2ad06b35b8d65f308b6b6b7bc6b00cf64eec9bcb
ixmn <zametaeva.dasha@gmail.com>
2024-01-24 11:43:22 +0400

    fix (plugins) удалить лог

--- src/plugins/componentsLoader.js
+++ src/plugins/componentsLoader.js
@@ -4,7 +4,6 @@ const modules = import.meta.glob("/src/zoo/*.vue", { eager: true });
 export default function loadComponents(app) {
   for (const path in modules) {
     const componentName = path.split("/").at(-1).split(".")[0];
-    console.log(componentName)
     app.component(`${componentName}`, modules[path].default);
   }
 }
