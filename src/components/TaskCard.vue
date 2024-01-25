063aca0bd393ce1731a41a4f39cca5f4438a1111
ixmn <zametaeva.dasha@gmail.com>
2024-01-25 13:13:33 +0400

    imp (components) добавить стиля для карточки задач

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,3 +1,21 @@
 <template lang="pug">
 //- Отрисовка самих карточек
+.task-card--container
+  app-title haha
+  app-body-text haha
 </template>
+
+<style scoped>
+.task-card--container {
+  min-width: 300px;
+  border-radius: 20px;
+  border: 1px solid rgba(49, 50, 55, 0.80);
+  background: rgba(222, 225, 248, 0.20);
+
+  display: flex; 
+  align-items: center;
+  flex-direction: column;
+  padding: 25px;
+}
+</style>
+
