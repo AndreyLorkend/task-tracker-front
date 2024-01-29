90d63a751cfea262450791f2de38e4f353109234
ixmn <zametaeva.dasha@gmail.com>
2024-01-29 17:43:02 +0400

    add (components) добавить диалоговые окно добавления и удаления колонок

--- src/components/AddColumnDialog.vue
+++ src/components/AddColumnDialog.vue
@@ -1,8 +1,21 @@
 <script setup>
-import { computed } from "vue";
+import { computed, reactive } from "vue";
+import { useColumnStore } from '../store/column.store'
+
+const store = useColumnStore()
 
 const props = defineProps({
   visible: Boolean
+})
+
+const rules = reactive({
+  nameRequired: value => !!value || '',
+  colorRequired: value => !!value || ''
+})
+
+const dialog = reactive({
+  title: null, 
+  color: null
 })
 
 const emit = defineEmits(['close'])
@@ -15,11 +28,44 @@ const show = computed({
     if(!value) emit('close')
   }
 })
+
+function addNewColumn(dialog) {
+  if (dialog.title && dialog.color) {
+    store.addNewColumn(dialog)
+    emit('close')
+  }
+  dialog.title = null
+  dialog.color = null
+}
 </script>
 
 
 <template lang="pug">
 v-dialog( v-model="show" width="500" )
   v-card 
-    app-title Добавить новую колонку
+    .add-dialog--container
+      app-title Добавить новую колонку
+      v-text-field( v-model="dialog.title" label="Введите название колонки" hide-details :rules="[rules.nameRequired]" )
+      v-text-field( v-model="dialog.color" label="Введите цвет колонки" hide-details :rules="[rules.colorRequired]" )
+      .d-flex.justify-center
+        app-button.mr-3.add-button--color(@click="addNewColumn(dialog)") Добавить 
+        app-button.cancel-button--color(@click="emit('close')") Отменить
 </template>
+
+<style scoped>
+.add-dialog--container {
+  padding: 30px; 
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.add-button--color {
+  background-color: #EAFFD8;
+}
+
+.cancel-button--color {
+  background-color: #FFADA1;
+}
+</style>
---
+++ src/components/DeleteColumnDialog.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  visible: Boolean
+})
+
+const emit = defineEmits(['close', 'delete'])
+
+const show = computed({
+  get () {
+    return props.visible
+  },
+  set (value) {
+    if(!value) emit('close')
+  }
+})
+
+</script>
+
+<template lang="pug">
+v-dialog( v-model="show" width="500" )
+  v-card 
+    .add-dialog--container
+      app-title Вы действительно хотите удалить колонку?
+      .d-flex.justify-center
+        app-button.mr-3.add-button--color(@click="emit('delete')") Удалить
+        app-button.cancel-button--color(@click="emit('close')") Отменить
+</template>
+
+<style scoped>
+.add-dialog--container {
+  padding: 30px; 
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.add-button--color {
+  background-color: #EAFFD8;
+}
+
+.cancel-button--color {
+  background-color: #FFADA1;
+}
+</style>
